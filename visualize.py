587ee0dd67f5265dc9b8e5b39e2d0e7d93f88667
Daniel Bankmann <bankmann@skolem.math.tu-berlin.de>
2017-06-30 16:38:45 +0200

    modified visualiztion

--- visualize.py
+++ visualize.py
@@ -4,10 +4,12 @@ def plot_eigenvalues(alg_object, n = 20):
     plt.figure()
     plt.plot(np.abs(alg_object.largest_eigenvalues)[:n], 'x')
     plt.plot(np.abs(alg_object.smallest_eigenvalues)[:n], 'o')
+    plt.plot(np.sqrt(np.abs(alg_object.largest_eigenvalues)[:n]* np.abs(alg_object.smallest_eigenvalues)[:n]), 'H')
     plt.show()
 
-def log_plot_eigenvalues(alg_object, n=20):
+def log_plot_eigenvalues(alg_object, n=100):
     plt.figure()
     plt.loglog(np.abs(alg_object.largest_eigenvalues)[:n], 'x')
     plt.loglog(np.abs(alg_object.smallest_eigenvalues)[:n], 'o')
+    plt.loglog(np.sqrt(np.abs(alg_object.largest_eigenvalues)[:n]* np.abs(alg_object.smallest_eigenvalues)[:n]), 'h')
     plt.show()
