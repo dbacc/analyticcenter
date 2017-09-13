added examples added tests fixed bug with sign and computation of X_-

analyticcenter/direction.py
@@ -9,13 +9,13 @@ from functools import partial
 
 
 class DirectionAlgorithm(object):
-    debug = False
+    debug = True
     ac_object = None
     logger = logging.getLogger(__name__)
     system = None
     initial_X = None
     discrete_time = False
-    maxiter = 10
+    maxiter = 100
     search_direction = None
 
     def __init__(self, ac_object, discrete_time):
@@ -33,22 +33,26 @@ class DirectionAlgorithm(object):
             self.logger.info("Current step: {}\tResidual: {}".format(steps_count, residual))
             self.logger.debug("Current objective value (det(H(X))): {}".format(determinant))
             self.logger.debug("Current X:\n{}".format(X))
-            if np.real(determinant) < 0:
+            det = np.real(determinant)
+            if det < 0 :
                 self.logger.critical("Something went wrong. Determinant ist negative. Aborting...")
                 raise ValueError("Something went wrong. Determinant ist negative")
 
-        X = self.initial_X()
+        X, success_init = self.initial_X()
         self.logger.debug("Initial X:\n{}".format(X))
         F, P = self.ac_object._get_F_and_P(X)
+
         determinant = linalg.det(P) * self.ac_object._get_determinant_R(X)
         A_F = (self.system.A - self.system.B @ F)
         steps_count = 0
         residual = self.ac_object.get_residual(X, P, F, A_F, self.search_direction)
         Delta_residual = float("inf")
         # ipdb.set_trace()
-        while residual > self.ac_object.tol and Delta_residual > self.ac_object.tol and steps_count < self.maxiter:
+        while residual > self.ac_object.tol and Delta_residual > self.ac_object.rel_tol and steps_count < self.maxiter:
             # ipdb.set_trace()
-
+            if self.debug:
+                self.ac_object._get_H_matrix(X)
+            ipdb.set_trace()
             print_information(steps_count, residual, determinant, X)
             R = self.ac_object._get_R(X)
             self.logger.debug("Current Determinant of R: {}".format(linalg.det(R)))
@@ -63,7 +67,14 @@ class DirectionAlgorithm(object):
             determinant = linalg.det(P) * self.ac_object._get_determinant_R(X)
             steps_count += 1
         print_information(steps_count, residual, determinant, X)
-        return X
+        self.logger.info("Finished computation...")
+        if residual <= self.ac_object.tol or Delta_residual <= self.ac_object.tol:
+            self.ac_object.center = X
+            self.ac_object.A_F = A_F
+            return (X, True)
+        else:
+
+            return (X, False)
 
     def __call__(self, *args, **kwargs):
         raise NotImplementedError("Please Implement this method")
@@ -75,8 +86,16 @@ class NewtonDirection(DirectionAlgorithm):
 
     def __call__(self):
         self.logger.info("Computing Analytic Center with Newton approach")
-        self._directional_iterative_algorithm(
+        X, success =  self._directional_iterative_algorithm(
             direction=self._get_newton_direction)
+        if success:
+            self.logger.info("Computation of Analytic center with Newton approach was successful")
+            self.logger.debug("At the analytic center A_F is:\n{}".format(self.ac_object.A_F))
+            return X
+        else:
+            self.logger.critical("Computation of Analytic center was  not successful")
+            return X
+
 
     def _get_newton_direction(self, X0, P0, R0, A_F):
         A_F_hat, P0_root, S2 = self._transform_system2current_X0(A_F, P0, R0)
@@ -115,21 +134,26 @@ class NewtonDirectionMultipleDimensionsCT(NewtonDirection):
         self.logger.debug("Solution Delta:\n{}".format(Delta))
         Delta = np.reshape(Delta, [n, n])
         self.logger.debug("Reshaped Delta:\n{}".format(Delta))
+
         # check if indeed solution:
+
+
+
         if self.debug:
             self._check(A, S, Delta)
-
-        def _check(self, A, S, Delta):
-            res = - A @ Delta @ A - AAH @ Delta - A.H @ Delta @ A.H - Delta @ AAH.H - S @ Delta - Delta @ S - A - A.H
-            self.logger.debug("norm of the residual: {}".format(linalg.norm(res)))
-            det_factor = linalg.det(identity - Delta @ A - A.H @ Delta - Delta @ S @ Delta)
-            if det_factor < 1.:
-                self.logger.critical("det factor by newton step is less than 1: {}".format(det_factor))
-            else:
-                self.logger.debug("det factor by newton step: {}".format(det_factor))
-
         return Delta
 
+    def _check(self, A, S, Delta):
+        AAH = A @ A.H
+        n = self.system.n
+        identity = np.identity(n)
+        res = - A @ Delta @ A - AAH @ Delta - A.H @ Delta @ A.H - Delta @ AAH.H - S @ Delta - Delta @ S - A - A.H
+        self.logger.debug("norm of the residual: {}".format(linalg.norm(res)))
+        det_factor = linalg.det(identity - Delta @ A - A.H @ Delta - Delta @ S @ Delta)
+        if det_factor < 1.:
+            self.logger.critical("det factor by newton step is less than 1: {}".format(det_factor))
+        else:
+            self.logger.debug("det factor by newton step: {}".format(det_factor))
 
 class NewtonDirectionIterativeCT(NewtonDirectionMultipleDimensionsCT):
     maxiter_newton = 200
@@ -351,26 +375,48 @@ class InitialX(DirectionAlgorithm):
         if InitialX.X0 is None:
             self.logger.info('Computing initial X')
 
-            X_plus = self.riccati_solver(self.system.A, self.system.B, self.system.Q, self.system.R, self.system.S, np.identity(self.system.n))
+            X_plus = -self.riccati_solver(self.system.A, self.system.B, self.system.Q, self.system.R, self.system.S,
+                                         np.identity(self.system.n))
             Am = -self.system.A
-            Bm = -self.system.B
-            X_minus = self.riccati_solver(Am, Bm, self.system.Q, self.system.R, self.system.S, np.identity(self.system.n))
+            Bm = self.system.B
+            Sm = self.system.S
+            Qm = -self.system.Q
+            Rm = -self.system.R
+            X_minus = -self.riccati_solver(Am, Bm, Qm, Rm, Sm,
+                                          np.identity(self.system.n))
+
+            if np.isclose(linalg.norm(X_plus-X_minus), 0):
+                self.logger.critical("X_+ and X_- are (almost) identical: No interior!")
+            self.logger.debug("Eigenvalues of X_plus: {}".format(linalg.eigh(X_plus)[0]))
+            self.logger.debug("Eigenvalues of H(X_plus): {}".format(linalg.eigh(self.ac_object._get_H_matrix(X_plus))[0]))
+            self.logger.debug("Eigenvalues of X_minus: {}".format(linalg.eigh(X_minus)[0]))
+            self.logger.debug("Eigenvalues of H(X_minus): {}".format(linalg.eigh(self.ac_object._get_H_matrix(X_minus))[0]))
+            if self.debug:
+                self.ac_object._get_Hamiltonian()
+            # ipdb.set_trace()
             newton_direction = self.newton_direction
-            self.search_direction = (X_minus + X_plus)
+            self.search_direction = 0.5 * (X_minus + X_plus)
+            self.logger.debug("Eigenvalues of X_init_guess: {}".format(linalg.eigh(self.search_direction)[0]))
+            self.logger.debug(
+                "Eigenvalues of H(X_init_guess): {}".format(linalg.eigh(self.ac_object._get_H_matrix(self.search_direction))[0]))
             newton_direction.search_direction = self.search_direction
-            InitialX.X0 = -0.5 * self.search_direction  # We use negative definite notion of solutions for Riccati equation
+            InitialX.X0 = self.search_direction  # We use negative definite notion of solutions for Riccati equation
             self.logger.info("Improving Initial X with Newton approach")
 
             # ipdb.set_trace()
-            Xinit = self._directional_iterative_algorithm(direction=newton_direction._get_newton_direction)
+            Xinit, success = self._directional_iterative_algorithm(direction=newton_direction._get_newton_direction)
+
+            if not success:
+                self.logger.critical("Computation of initial X failed.")
+            else:
+                self.logger.debug("Eigenvalues of X_init: {}".format(linalg.eigh(Xinit)[0]))
+                self.logger.debug(
+                    "Eigenvalues of H(X_init): {}".format(linalg.eigh(self.ac_object._get_H_matrix(Xinit))[0]))
 
         else:
-            self.logger.info("initial X is already set")
+            self.logger.info("Initial X is already set")
             Xinit = self.X0
-
-        return Xinit
-
-
+        return Xinit, True
 class InitialXCT(InitialX):
     newton_direction = NewtonDirectionOneDimensionCT()
     riccati_solver = staticmethod(lambda *args: control.care(*args)[0])

analyticcenter/linearsystem.py
@@ -1,6 +1,9 @@
 import numpy as np
 from scipy import linalg
 import logging
+import control
+import misc.misc as misc
+
 
 class LTI(object):
     """Describes an LTI system"""
@@ -25,7 +28,7 @@ class OptimalControlSystem(LTI):
         self.S = np.asmatrix(S)
         self.R = np.asmatrix(R)
         self.__initH0()
-        self._check_positivity(self.H0)
+        misc.check_positivity(self.H0, "H_0")
 
     def save(self):
         np.save('example-n-{}-m-{}'.format(self.n, self.m), [self.A, self.B, self.C, self.D, self.Q, self.S, self.R])
@@ -33,11 +36,12 @@ class OptimalControlSystem(LTI):
     def __initH0(self):
         self.H0 = np.bmat([[self.Q, self.S], [self.S.transpose(), self.R]])
 
-    def _check_positivity(self, M):
-        try:
-            linalg.cholesky(M)
-            self.logger.info('Matrix {} is positive definite'.format(M))
-            return True
-        except linalg.LinAlgError as err:
-            self.logger.info('Matrix {} is not positive definite'.format(M))
-            return False
+
+
+    def check_passivity(self):
+        ricc = control.care(self.A, self.B, self.Q, self.R, self.S, np.identity(self.n))
+        if misc.check_positivity(ricc[0], 'X'):
+            self.logger.info("System is passive, if also stable")
+        else:
+            self.logger.critical("System cannot be stabilized (in particular unstable)")
+            raise BaseException("System cannot be stabilized (in particular unstable)")

examples/cheby_filter.py
@@ -1,31 +1,22 @@
 import control
 import numpy as np
 from analyticcenter.linearsystem import OptimalControlSystem
-
-RR = 100.  # Resistor Value
-L = 0.1e-1  # Inductance
-C = 1.e-1 # Capacitor
-
-p = 0  # Number of systems to connect
-
-num = np.array([1 / (L * C)])
-den = np.array([1, RR / L, 1 / (L * C)])
-#
-# num = np.array([1 / (RR * C), 0])
-# den = np.array([1, 1 / (RR * C), 1 / (L * C)])
+from scipy.signal import cheby2
 
 
+
+
+num, den = cheby2(6,30, [1000,200000], btype='bandpass', analog=True)
 tf = control.tf(num, den)
 
 ss = tf
-for i in range(p):
-    ss = control.series(ss, tf)
+
 
 sys = control.tf2ss(ss)
 A = sys.A
 B = sys.B
 C = np.asmatrix(sys.C)
-D = np.asmatrix(sys.D) + 0.1
+D = np.asmatrix(sys.D)
 
 
 # D = np.matrix([1])

misc/misc.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import linalg
-
+import logging
+logger = logging.getLogger()
 
 
 
@@ -17,3 +18,16 @@ def schur_complement(X, n, mode='upper'):
     return complement
 
 
+def check_positivity(M, M_name=""):
+    logger.debug("Checking positive definiteness for matrix {}:\n{}".format(M_name, M))
+    try:
+        linalg.cholesky(M)
+        logger.debug('Matrix {} is non-negative'.format(M_name))
+        return True
+    except linalg.LinAlgError as err:
+        lmin = np.min(linalg.eigh(M)[0])
+        if lmin >=0 or np.isclose(lmin,0):
+            logger.warning('Matrix {} seems to be non-negative, but Cholesky factorization failed due to roundoff errors'.format(M_name))
+        else:
+            logger.critical('Matrix {} is not non-negative'.format(M_name))
+        return False